3a9d3480a8166ff4b025b812b90d184b35015e0d
Dominik Hadl <nick@dynamicdust.com>
2016-06-07 16:00:46 +0200

    Add support for all platforms

--- UserManagerType/Supporting Files/UserManagerType.h
+++ UserManagerType/Supporting Files/UserManagerType.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016 Nodes. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for UserManagerType.
 FOUNDATION_EXPORT double UserManagerTypeVersionNumber;
